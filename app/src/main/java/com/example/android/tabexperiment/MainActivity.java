07a4ab460d351102b2491147a411b7f2f7f94723
ahmed-razic <ahmed_razic@gmail.com>
2021-02-21 10:33:52 +0100

    added tablayout

--- app/src/main/java/com/example/android/tabexperiment/MainActivity.java
+++ app/src/main/java/com/example/android/tabexperiment/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android.tabexperiment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 }
